81637afa827fc12bd5468d2dc3c995016e289da2
Christopher Majewski <chris.majewski@gmail.com>
2016-05-10 11:29:35 -0400

    Cleanup and PEP8

--- harvest_hnap.py
+++ harvest_hnap.py
@@ -37,68 +37,78 @@ import re
 from lxml import etree
 import docopt
 
-# Connection variables
-csw_url = 'csw.open.canada.ca/geonetwork/srv/csw'
-csw_user = None
-csw_passwd = None
 
-proxy_protocol = None
-proxy_url = None
-proxy_user = None
-proxy_passwd = None
+def main():
+    # Connection variables
+    csw_url = 'csw.open.canada.ca/geonetwork/srv/csw'
+    csw_user = None
+    csw_passwd = None
 
-# Or read from a .ini file
-harvester_file = 'config/harvester.ini'
-if os.path.isfile(harvester_file):
-    from ConfigParser import ConfigParser
+    proxy_protocol = None
+    proxy_url = None
+    proxy_user = None
+    proxy_passwd = None
 
-    ini_config = ConfigParser()
+    # Or read from a .ini file
+    harvester_file = 'config/harvester.ini'
+    if os.path.isfile(harvester_file):
+        from ConfigParser import ConfigParser
 
-    ini_config.read(harvester_file)
+        ini_config = ConfigParser()
 
-    csw_url = ini_config.get('csw', 'url')
-    if ini_config.has_option('csw', 'username'):
-        csw_user = ini_config.get('csw', 'username')
-        csw_passwd = ini_config.get('csw', 'password')
+        ini_config.read(harvester_file)
 
-    proxy_protocol = ini_config.get('proxy', 'protocol')
-    proxy_url = ini_config.get('proxy', 'url')
-    if ini_config.has_option('proxy', 'username'):
-        proxy_user = ini_config.get('proxy', 'username')
-        proxy_passwd = ini_config.get('proxy', 'password')
+        csw_url = ini_config.get(
+            'csw', 'url')
+        if ini_config.has_option('csw', 'username'):
+            csw_user = ini_config.get(
+                'csw', 'username')
+            csw_passwd = ini_config.get(
+                'csw', 'password')
 
-    records_per_request = int(ini_config.get('processing', 'records_per_request'))
-    start_date = ini_config.get('processing', 'start_date')
+        proxy_protocol = ini_config.get(
+            'proxy', 'protocol')
+        proxy_url = ini_config.get(
+            'proxy', 'url')
+        if ini_config.has_option('proxy', 'username'):
+            proxy_user = ini_config.get(
+                'proxy', 'username')
+            proxy_passwd = ini_config.get(
+                'proxy', 'password')
 
-# If your supplying a proxy
-if proxy_url:
-    # And your using authentication
-    if proxy_user and proxy_passwd:
-        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        password_mgr.add_password(None, proxy_url, proxy_user, proxy_passwd)
-        proxy_auth_handler = urllib2.ProxyBasicAuthHandler(password_mgr)
-    # or even if your not
+        records_per_request = int(ini_config.get(
+            'processing', 'records_per_request'))
+        start_date = ini_config.get('processing', 'start_date')
+
+    # If your supplying a proxy
+    if proxy_url:
+        # And your using authentication
+        if proxy_user and proxy_passwd:
+            password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            password_mgr.add_password(
+                None, proxy_url, proxy_user, proxy_passwd)
+            proxy_auth_handler = urllib2.ProxyBasicAuthHandler(password_mgr)
+        # or even if your not
+        else:
+            proxy_auth_handler = urllib2.ProxyHandler(
+                {proxy_protocol: proxy_url})
+
+        opener = urllib2.build_opener(proxy_auth_handler)
+        urllib2.install_opener(opener)
+
+    # Fetch the data
+    # csw = CatalogueServiceWeb(
+    #   'http://csw_user:csw_pass@csw_url/geonetwork/srv/csw')
+    if csw_user and csw_passwd:
+        csw = CatalogueServiceWeb(
+            'http://'+csw_url,
+            username=csw_user,
+            password=csw_passwd,
+            timeout=20)
     else:
-        proxy_auth_handler = urllib2.ProxyHandler({proxy_protocol: proxy_url})
+        csw = CatalogueServiceWeb('http://'+csw_url, timeout=20)
 
-    opener = urllib2.build_opener(proxy_auth_handler)
-    urllib2.install_opener(opener)
-
-# Fetch the data
-# csw = CatalogueServiceWeb(
-#   'http://csw_user:csw_pass@csw_url/geonetwork/srv/csw')
-if csw_user and csw_passwd:
-    csw = CatalogueServiceWeb(
-        'http://'+csw_url,
-        username=csw_user,
-        password=csw_passwd,
-        timeout=20)
-else:
-    csw = CatalogueServiceWeb('http://'+csw_url, timeout=20)
-
-#"47","date_modified","Date Modified","Date de modification ","M","S","YYYY-MM-DD","manual","gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:date/gmd:CI_Date",""
-
-request_template = """<?xml version="1.0"?>
+    request_template = """<?xml version="1.0"?>
 <csw:GetRecords
     xmlns:csw="http://www.opengis.net/cat/csw/2.0.2"
     service="CSW"
@@ -126,8 +136,6 @@ request_template = """<?xml version="1.0"?>
 </csw:GetRecords>
 """
 
-def main():
-    #print "Start Process"
     # Is there a specified start date
     if arguments['-f']:
         start_date = arguments['-f']
@@ -136,7 +144,6 @@ def main():
     next_record = 1
     request_another = True
     while request_another:
-        #print "Fetch Next"
         request_another = False
 
         # Filter records into latest updates
@@ -145,16 +152,21 @@ def main():
         # For now it's good ol' Kitchen Sink
         #
         # from owslib.fes import PropertyIsGreaterThanOrEqualTo
-        # modified = PropertyIsGreaterThanOrEqualTo('apiso:Modified', '2015-04-04')
+        # modified = PropertyIsGreaterThanOrEqualTo(
+        #   'apiso:Modified',
+        #   '2015-04-04'
+        # )
         # csw.getrecords2(constraints=[modified])
         #
         # Kitchen Sink is the valid HNAP, we need HNAP for R1 to debug issues
         # This filter was supplied by EC, the CSW service technical lead
         current_request = request_template % (
             records_per_request,
-            next_record
+            next_record,
+            start_date
         )
-        #(active_page*records_per_request)+1
+
+        # (active_page*records_per_request)+1
         csw.getrecords2(format='xml', xml=current_request)
         active_page += 1
 
@@ -166,33 +178,23 @@ def main():
         # Parse the root and itterate over each record
         records = fetchXMLArray(root, records_root)
 
-# <?xml version="1.0" encoding="UTF-8"?>
-# <csw:GetRecordsResponse xmlns:csw="http://www.opengis.net/cat/csw/2.0.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/cat/csw/2.0.2 http://schemas.opengis.net/csw/2.0.2/CSW-discovery.xsd">
-#   <csw:SearchStatus timestamp="2016-05-07T19:07:09" />
-#   <csw:SearchResults numberOfRecordsMatched="38" numberOfRecordsReturned="10" elementSet="full" nextRecord="11">
-#     <gmd:MD_Metadata xmlns:gmd="http://www.isotc211.org/2005/gmd" xmlns:gco="http://www.isotc211.org/2005/gco" xmlns:srv="http://www.isotc211.org/2005/srv" xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:gfc="http://www.isotc211.org/2005/gfc" xmlns:gmi="http://www.isotc211.org/2005/gmi" xmlns:gmx="http://www.isotc211.org/2005/gmx" xmlns:gsr="http://www.isotc211.org/2005/gsr" xmlns:gss="http://www.isotc211.org/2005/gss" xmlns:gts="http://www.isotc211.org/2005/gts" xmlns:geonet="http://www.fao.org/geonetwork">
-
         timestamp = fetchXMLAttribute(
-            records[0],
-            "csw:SearchStatus",
+            records[0], "csw:SearchStatus",
             "timestamp")[0]
         number_of_records_matched = int(fetchXMLAttribute(
-            records[0],
-            "csw:SearchResults",
+            records[0], "csw:SearchResults",
             "numberOfRecordsMatched")[0]
         )
         number_of_records_returned = int(fetchXMLAttribute(
-            records[0],
-            "csw:SearchResults",
+            records[0], "csw:SearchResults",
             "numberOfRecordsReturned")[0]
         )
         next_record = int(fetchXMLAttribute(
-            records[0],
-            "csw:SearchResults",
+            records[0], "csw:SearchResults",
             "nextRecord")[0]
         )
 
-        if next_record > number_of_records_matched:
+        if next_record > number_of_records_matched or next_record == 0:
             pass
         else:
             request_another = True
@@ -204,8 +206,8 @@ def main():
         #    print '* '+csw.records[rec].title
         # Till then we need to collect and dump the response from the CSW
 
-        # No use minimizing the XML to try to create a XML Lines file as the data
-        # has carriage returns.
+        # No use minimizing the XML to try to create a XML Lines file as the
+        # data has carriage returns.
         # parser = etree.XMLParser(remove_blank_text=True)
         # elem = etree.XML(csw.response, parser=parser)
         # print etree.tostring(elem)
@@ -213,21 +215,24 @@ def main():
         # Output the harvested page
         print csw.response
 
+
 ##################################################
 # XML Extract functions
 # fetchXMLArray(objectToXpath, xpath)
 # fetchXMLAttribute(objectToXpath, xpath, attribute)
 
-# Fetch an array which may be subsections
+
 def fetchXMLArray(objectToXpath, xpath):
+# Fetch an array which may be subsections
     return objectToXpath.xpath(xpath, namespaces={
         'gmd': 'http://www.isotc211.org/2005/gmd',
         'gco': 'http://www.isotc211.org/2005/gco',
         'gml': 'http://www.opengis.net/gml/3.2',
         'csw': 'http://www.opengis.net/cat/csw/2.0.2'})
 
-# Fetch an attribute instead of a an element
+
 def fetchXMLAttribute(objectToXpath, xpath, attribute):
+# Fetch an attribute instead of a an element
     # Easy to miss this, clean and combine
     clean_xpath = xpath.rstrip('/')
     clean_attribute = xpath.lstrip('@')
